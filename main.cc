9d3a66d1766999e8d7b1042d5ea40f7c65f2cc13
Ty <68357354+jsxa@users.noreply.github.com>
2020-07-23 22:52:40 -0400

    yes

--- main.cc
+++ main.cc
@@ -3,13 +3,32 @@
 
 using namespace std;
 
+double power(double base, int exponent)// declaring, defining.
+{
+    double result = 1;
+    for(int i = 0; i < exponent; i++)
+    {
+        result = result * base;
+    }
+    return result;
+}
+
+
+void print_pow(double base, int exponent)
+{
+    double myPower = power(base, exponent);
+    cout << base << " raised to the " << exponent << " power is" << myPower << ".\n";
+}
+
+
 int main()
 {
+    double base;
+    int exponent;
     int base, exponent;
     cout << "What is the base?: ";
     cin >> base;
     cout << "what is the exponent: ";
     cin >> exponent;
-    double power = pow(base, exponent);
-    cout << power << endl;
+    print_pow(base, exponent);
 }
